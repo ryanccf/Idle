ad9565e4653fba80b0f03576cab52cfb9cec61bd
RyanFaegre <ryan.faegre@americanfinancing.net>
2021-01-13 20:04:09 -0600

    Add Wages Add Wages and payroll functionality. Add an indicator of the current quarterly payroll due.

--- Game.gd
+++ Game.gd
@@ -4,7 +4,9 @@ signal send_lead
 
 var counter = 0
 var leads = 30
+var lead_price = 1
 var agents = 0
+var agent_price = 100
 var ticks = 0
 var year = 1969
 var quarter = 1
@@ -12,6 +14,7 @@ var multiplier = 1
 var base_multiplier = 1.0
 var max_multiplier = 10
 var call_value = 25
+var wages = 0
 
 var Agent = preload("res://Agent.tscn")
 
@@ -21,6 +24,7 @@ onready var Agents = $Background/Agents
 onready var Year = $Background/Year
 onready var Quarter = $Background/Quarter
 onready var Multiplier = $Background/Multiplier
+onready var Wages = $Background/Wages
 
 const TICKS_PER_QUARTER = 90
 const VICTORY_CONDITION = 400000
@@ -40,14 +44,23 @@ func _on_BuyLeadsButton_pressed():
 	refresh_view()
 
 func _on_BuyAgentButton_pressed():
-	if counter > 99:
+	if counter >= agent_price:
 		get_node("CrashSound").play()
-		counter -= 100
+		counter -= agent_price
 		agents += 1
+		update_wages()
 		add_agent()
+	refresh_view()
 
 func _on_Agent_sale(money):
 	add_money(stepify(money * multiplier, 0.01))
+
+func _on_AdvertiseButton_pressed():
+	if multiplier <= max_multiplier:
+		counter -= lead_price
+		get_node("StickSound").play()
+		multiplier += 0.1
+		refresh_view()
 
 func _on_Tick_timeout():
 	ticks += 1
@@ -56,6 +69,12 @@ func _on_Tick_timeout():
 	send_leads()
 	calc_multiplier()
 	refresh_view()
+
+func update_wages():
+	wages = (agents * agent_price)
+
+func pay_payroll():
+	counter -= wages
 
 func send_leads():
 	if leads >= agents:
@@ -69,6 +88,7 @@ func refresh_view():
 	Quarter.text = String(quarter)
 	Year.text = String(year)	
 	Multiplier.text = String(multiplier)
+	Wages.text = String(wages)
 
 func dial():
 	get_node("FloorTomSound").play()
@@ -79,7 +99,6 @@ func dial():
 		if random_number == 1:
 			get_node("SpinnerSound").play()
 			counter += stepify(call_value * multiplier, 0.01)
-	refresh_view()
 
 func add_money(money):
 	counter += money
@@ -102,12 +121,7 @@ func check_quarter():
 	if ticks > TICKS_PER_QUARTER:
 		ticks = 0
 		quarter += 1
+		pay_payroll()
 		if quarter > 4:
 			quarter = 1
 			year += 1
-
-func _on_AdvertiseButton_pressed():
-	if multiplier <= max_multiplier:
-		get_node("StickSound").play()
-		multiplier += 0.1
-		refresh_view()
